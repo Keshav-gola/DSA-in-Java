57752a7552a18f54caef73fd28a608b759b939db
Keshav Gola <keshavgola285@gmail.com>
2024-07-29 17:48:15 +0530

    Adding new function - Trapping Rainwater

--- ARRAY.java
+++ ARRAY.java
@@ -1,6 +1,30 @@
 package Java;
 import java.util.*;
 public class ARRAY {
+    public static int tapping_rainwater(int arr[]){
+        int n = arr.length;
+        //Calculate left max boundary
+        int leftmax[] = new int[n];
+        leftmax[0] = arr[0];
+        for(int i = 1; i<n; i++){
+            leftmax[i] = Math.max(arr[i],leftmax[i-1]);
+        }
+
+        //Calculate right max boundary
+        int rightmax[] = new int[n];
+        rightmax[n-1] = arr[n-1];
+        for(int i = n-2; i>=0; i--){
+            rightmax[i] = Math.max(arr[i], rightmax[i+1]);
+        }
+
+        int trappedWater = 0;
+        for(int i = 0; i<n; i++){
+            int waterlevel = Math.min(rightmax[i],leftmax[i]);
+            trappedWater += waterlevel - arr[i];
+        }
+
+        return trappedWater;
+    }
     public static void reverse_arry(int arr[]){
         int start = 0, last = arr.length-1;
 
@@ -14,11 +38,13 @@ public class ARRAY {
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int arr[] = {5,6,7,14,3};
-        reverse_arry(arr);
-        for(int i =0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
-        }
+        int arr[] = {4,2,0,6,3,2,5};
+        // reverse_arry(arr);
+        // for(int i =0; i<arr.length; i++){
+        //     System.out.print(arr[i]+" ");
+        // }
 
+        int waterValue = tapping_rainwater(arr);
+        System.out.println(waterValue);
     }
 }
