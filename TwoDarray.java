d8fc10e2cddb1ea8eaff6e4064123265b7ade5da
Keshav Gola <keshavgola285@gmail.com>
2024-07-30 18:51:12 +0530

    Adding new function - Diagonal Sum

--- TwoDarray.java
+++ TwoDarray.java
@@ -1,6 +1,33 @@
 package Java;
 import java.util.*;
 public class TwoDarray {
+    public static int diagonal (int arr[][]){
+        int sum = 0; 
+
+        //Brut_Force Method
+        // for(int i = 0; i<=arr.length-1; i++){
+        //     for(int j = 0; j<arr[0].length; j++){
+        //         if(i == j) {
+        //             sum += arr[i][j];
+        //         }
+        //         else if(i+j == arr.length-1){
+        //             sum+= arr[i][j];
+        //         }
+        //     }
+        // }
+
+        //Advance Method
+        for(int i = 0; i<= arr.length-1; i++){
+            //Primary diagonal 
+            sum += arr[i][i];
+            //Secondary Diagonal
+            if(i != arr.length-1-i){
+                sum += arr[i][arr.length-1-i];
+            }
+        }
+
+        return sum;
+    }
     public static void sprial(int arr[][]){
         int startRow = 0 , startCol = 0;
         int endRow = arr.length-1, endCol = arr[0].length-1;
@@ -42,12 +69,14 @@ public class TwoDarray {
                 arr[i][j] = sc.nextInt();
             }
         }
-        sprial(arr);
+        //sprial(arr);
         // for(int i = 0; i<n; i++){
         //     for(int j = 0; j<m; j++){
         //         System.out.print(arr[i][j]+" ");
         //     }
         //     System.out.println();
         // }
+        int sum = diagonal(arr);
+        System.out.println(sum);
     }
 }
