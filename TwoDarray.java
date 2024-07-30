8d6e42888fdf15f7fd2ac46d77a03d8935bc90d0
Keshav Gola <keshavgola285@gmail.com>
2024-07-30 18:09:29 +0530

    Adding new Algo for - Sprial Matrix

--- TwoDarray.java
+++ TwoDarray.java
@@ -1,6 +1,37 @@
 package Java;
 import java.util.*;
 public class TwoDarray {
+    public static void sprial(int arr[][]){
+        int startRow = 0 , startCol = 0;
+        int endRow = arr.length-1, endCol = arr[0].length-1;
+
+        while(startRow <= endRow && startCol <= endCol){
+
+            //top
+            for(int j = startCol; j<= endCol; j++){
+                System.out.print(arr[startRow][j]+" ");
+            }
+
+            //right
+            for(int i = startRow+1; i<= endRow; i++){
+                System.out.print(arr[i][endCol]+" ");
+            }
+
+            //bottom
+            for(int j= endCol-1; j>=startCol; j--){
+                System.out.print(arr[endRow][j]+" ");
+            }
+
+            //left
+            for(int i =endRow-1; i>startRow; i--){
+                System.out.print(arr[i][startCol]+" ");
+            }
+            startRow++;
+            startCol++;
+            endCol--;
+            endRow--;
+        }
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
@@ -11,11 +42,12 @@ public class TwoDarray {
                 arr[i][j] = sc.nextInt();
             }
         }
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
+        sprial(arr);
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         System.out.print(arr[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
