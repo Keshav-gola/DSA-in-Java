3193fe851a3da9accc4553a36879c93f170b2927
Keshav Gola <keshavgola285@gmail.com>
2024-07-30 10:25:02 +0530

    Adding new function - Bubble Sort

--- ARRAY.java
+++ ARRAY.java
@@ -1,6 +1,17 @@
 package Java;
 import java.util.*;
 public class ARRAY {
+    public static void bubble_sort(int arr[]){
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-1-i; j++ ){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
     public static int sub_arry(int arr[]){
         int sum , maxSubArry = Integer.MIN_VALUE;
         for(int i = 0; i<arr.length; i++){
@@ -64,7 +75,11 @@ public class ARRAY {
 
         // int waterValue = tapping_rainwater(arr);
         // System.out.println(waterValue);
-        int total = sub_arry(arr);
-        System.out.println(total);
+        // int total = sub_arry(arr);
+        // System.out.println(total);
+        bubble_sort(arr);
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]);
+        }
     }
 }
