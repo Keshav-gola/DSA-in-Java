b09bdc33cf8a513330d14b6ea491179833ee9693
Keshav Gola <keshavgola285@gmail.com>
2024-08-01 18:30:50 +0530

    Adding new file - Strings

---
+++ Strings.java
@@ -0,0 +1,10 @@
+package Java;
+import java.util.*;
+public class Strings {
+    public static void main(String args[]){
+        String str = "keshav gola";
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        System.out.println(str+"\n"+str1);
+    }
+}
