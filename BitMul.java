bf39c23d9840aa522f46dc11d0b3b707bf407ce7
Keshav Gola <keshavgola285@gmail.com>
2024-08-02 20:04:39 +0530

    Create new file - BitMultiplication

---
+++ BitMul.java
@@ -0,0 +1,15 @@
+package Java;
+import java.util.*;
+public class BitMul {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~a);
+    }
+}
