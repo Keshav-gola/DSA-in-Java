c04ead3285556d78c0613f04578dfa2e27fe5141
Keshav Gola <keshavgola285@gmail.com>
2024-08-02 20:43:42 +0530

    Adding new Function for - Check Even or Odd

--- BitMul.java
+++ BitMul.java
@@ -1,6 +1,15 @@
 package Java;
 import java.util.*;
 public class BitMul {
+    public static void evenOrodd(int num){
+        num = num & 1;
+        if(num == 0){
+            System.out.println("This number is Even");
+        }
+        else{
+            System.out.println("This number is odd");
+        }
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
@@ -21,6 +30,11 @@ public class BitMul {
 
         //Binary Left Shift <<
         System.out.println(a<<b);
+
+        //Binary Right Shift >>
+        System.out.println(a>>b);
+
+        evenOrodd(a);
 
     }
 }
