29c9f305ac4e517aa4e07769406a13c083be9f44
Keshav Gola <keshavgola285@gmail.com>
2024-08-04 09:59:26 +0530

    Adding new function - Factioral

--- Recursion.java
+++ Recursion.java
@@ -1,6 +1,14 @@
 package Java;
 import java.util.*;
 public class Recursion {
+    public static int fact(int num){
+        if(num == 0){
+            return 1;
+        }
+        int fnm1 = fact(num-1);
+        int fn = num * fnm1;
+        return fn;
+    }
     public static void cout(int num){
         if(num == 1){
             System.out.print("1");
@@ -18,6 +26,7 @@ public class Recursion {
         printing(num - 1);
     }
     public static void main(String args[]) {
-        cout(10);
+        //cout(10);
+        System.out.println(fact(5));
     }
 }
