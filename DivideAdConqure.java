7120a7ebdae127340ba9440f0eef21c9e00afa8d
Keshav-gola <keshavgola285@gmail.com>
2024-08-07 18:55:36 +0530

    Adding new function - Quick Sort

--- DivideAdConqure.java
+++ DivideAdConqure.java
@@ -6,6 +6,34 @@ public class DivideAdConqure {
             System.out.print(arr[i]+" ");
         }
     }
+    public static void quickSort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        //last element
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx-1);
+        quickSort(arr, pIdx+1, ei);
+    }
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si - 1;
+        
+        for(int j = si; j<ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
     public static void meargshor(int arr[], int si, int ei){
         //Base
         if(si >= ei){
@@ -50,7 +78,8 @@ public class DivideAdConqure {
     }
     public static void main(String args[]){
         int arr[] = {8,5,7,6,9,4,2};
-        meargshor(arr, 0, arr.length-1);
+        //meargshor(arr, 0, arr.length-1);
+        quickSort(arr, 0, arr.length-1);
         printArr(arr);
     }
 }
