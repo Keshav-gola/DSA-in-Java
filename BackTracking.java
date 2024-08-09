bf9895baec725be51bca2c1e39a67bc1f04d3a87
Keshav-gola <keshavgola285@gmail.com>
2024-08-09 05:54:18 +0530

    Adding new function - SubSet String

--- BackTracking.java
+++ BackTracking.java
@@ -1,8 +1,20 @@
 package Java;
 
-public class BackTraking {
+public class BackTracking {
+    public static void subSetString(String str, String ans, int i ){
+        if(i == str.length()){
+            System.out.println(ans);
+            return;
+        }
+        //yes
+        subSetString(str, ans + str.charAt(i), i+1);
+        // No
+        subSetString(str, ans, i+1);
+    }
     public static void main(String args[]){
-        
+        String str = "abc";
+        String ans = "";
+        subSetString(str, ans, 0);
     }
     
 }
