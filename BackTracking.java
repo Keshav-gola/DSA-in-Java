0c0a4ff8c3c30afb1c06d126f4f96581f9382247
Keshav-gola <keshavgola285@gmail.com>
2024-08-10 06:34:00 +0530

    Solveing nQueen Problem

--- BackTracking.java
+++ BackTracking.java
@@ -1,5 +1,5 @@
 package Java;
-
+import java.util.*;
 public class BackTracking {
     public static void purmentation (String str, String ans){
         if(str.length() == 0){
@@ -10,6 +10,51 @@ public class BackTracking {
             char curr = str.charAt(i);
             String Newstr = str.substring(0, i) + str.substring(i+1);
             purmentation(Newstr, ans+curr);
+        }
+    }
+    public static boolean isSafe(char board[][], int row, int col){
+        //vertical upside
+        for(int i = row-1; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //left diagonal upside
+        for(int i= row-1, j=col-1; i>=0 && j>=0; i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //right diagonal upside
+        for(int i = row-1, j= col+1; i>=0 && j<board.length; i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nQueen(char board [][], int row){
+        //base
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        //recursion
+        for(int j = 0; j<board.length; j++){
+            if(isSafe(board,row,j)){
+                board[row][j] = 'Q';
+                nQueen(board, row+1);
+                board[row][j] = 'x';
+            }
+        }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("---------new Board---------");
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board.length; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
         }
     }
     public static void subSetString(String str, String ans, int i ){
@@ -23,10 +68,19 @@ public class BackTracking {
         subSetString(str, ans, i+1);
     }
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
         String str = "abc";
         String ans = "";
         subSetString(str, ans, 0);
         purmentation(str, ans);
+        int n = sc.nextInt();
+        char board[][] = new char[n][n];
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board.length; j++){
+                board[i][j] = 'x';
+            }
+        }
+        nQueen(board, 0);
     }
     
 }
