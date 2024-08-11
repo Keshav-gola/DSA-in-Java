75784737f52de34208bf9c16f479e6c2e6ce0027
Keshav-gola <keshavgola285@gmail.com>
2024-08-11 20:09:23 +0530

    Adding new function - Sudoku Solver

--- BackTracking.java
+++ BackTracking.java
@@ -1,6 +1,67 @@
 package Java;
 import java.util.*;
 public class BackTracking {
+    public static boolean isSafe(int sudoku[][], int row, int col, int digit){
+        //column
+        for(int i = 0; i <= 8; i++ ){
+            if(sudoku[i][col] == digit){
+                return false;
+            }
+        }
+
+        //row
+        for(int j = 0; j<= 8; j++){
+            if(sudoku[row][j] == digit){
+                return false;
+            }
+        }
+
+        //grid
+        int sr = (row/3) * 3;
+        int sc = (col/3) * 3;
+
+        for(int i = sr; i<sr+3; i++){
+            for(int j = sc; j<sc+3; j++){
+                if(sudoku[i][j] == digit){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean sudokuSolver(int sudoku[][], int row, int col){
+        if(row == 9){
+            return true;
+        }
+
+        int nextRow = row, nextCol = col+1;
+        if(col+1 == 9){
+            nextRow = row +1;
+            nextCol = 0;
+        }
+        if(sudoku[row][col] != 0){
+            return sudokuSolver(sudoku, nextRow, nextCol);
+        }
+
+        for(int digit = 1; digit <= 9; digit++){
+            if(isSafe(sudoku, row, col, digit)){
+                sudoku[row][col] = digit;
+                if(sudokuSolver(sudoku, nextRow, nextCol)){
+                    return true;
+                }
+                sudoku[row][col] = 0;
+            }
+        }
+        return false;
+    }
+    public static void printSudoku(int sudoku[][]){
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j<9; j++){
+                System.out.print(sudoku[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static int gridWay(int i, int j, int n, int m){
         if(i == n-1 && j == m-1){
             return 1;
@@ -96,7 +157,21 @@ public class BackTracking {
         System.out.println("Total possible ways are :- "+count);
         int a = 3, m = 3;
         System.out.println(gridWay(0, 0, a, m));
-
+        int sudoku[][]= {{0,5,0,0,4,0,0,0,0},
+                         {2,7,0,0,9,0,0,0,3},
+                         {0,0,1,0,0,5,0,9,0},
+                         {1,0,8,7,0,0,6,0,0},
+                         {7,0,0,0,0,0,0,0,1},
+                         {0,0,5,0,0,4,8,0,7},
+                         {0,1,0,4,0,0,5,0,0},
+                         {8,0,0,0,3,0,0,6,2},
+                         {0,0,0,0,6,0,0,8,0}};
+        if(sudokuSolver(sudoku, 0, 0)){
+            System.out.println("Solution exists");
+            printSudoku(sudoku);
+        }else{
+            System.out.println("Solution does not exist");
+        }
     }
     
 }
