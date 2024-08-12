6aaaf26bcb9595c37d9086ad3e6c7baa8edc05b6
Keshav-gola <keshavgola285@gmail.com>
2024-08-12 22:16:40 +0530

    Using Array_list Methods

--- Array_List.java
+++ Array_List.java
@@ -1,5 +1,31 @@
 package Java;
+import java.util.ArrayList;
+public class Array_List {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
 
-public class ArrayList {
-    
+        //Adding Element 
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        list.add(10);
+        list.add(23);
+        list.add(32);
+        System.out.println(list);
+
+        //Get  Element
+        int element = list.get(2);
+        System.out.println(element);
+
+        //Removing Element
+        list.remove(4);
+        System.out.println(list);
+
+        //Set Element
+        list.set(4, 24);
+        System.out.println(list);
+
+        System.out.println(list.contains(24));
+        System.out.println(list.contains(18));
+    }
 }
