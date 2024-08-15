a7b6e9187a8b16b607bfa1c4d4147d83cdd53f93
Keshav-gola <keshavgola285@gmail.com>
2024-08-15 23:18:23 +0530

    Adding new class Node

--- LinkedList.java
+++ LinkedList.java
@@ -1,12 +1,14 @@
 package Java;
 
 public class LinkedList {
-    int data;
-    LinkedList next;
-    
-    public LinkedList(int data){
-        this.data = data;
-        this.next = null;
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
     }
     public static void main(String args[]){
 
