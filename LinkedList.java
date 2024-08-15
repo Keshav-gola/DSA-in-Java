c47a514444e77577555342162a3a3b8639445200
Keshav-gola <keshavgola285@gmail.com>
2024-08-15 22:59:05 +0530

    Creat a new file - Linked List

---
+++ LinkedList.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class LinkedList {
+    int data;
+    LinkedList next;
+    
+    public LinkedList(int data){
+        this.data = data;
+        this.next = null;
+    }
+    public static void main(String args[]){
+
+    }
+}
