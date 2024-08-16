7604a9acf0b6d169f37e2f9c730dc2f0998e7872
Keshav-gola <keshavgola285@gmail.com>
2024-08-16 22:45:36 +0530

    Using Constructor Overloading

--- Oops.java
+++ Oops.java
@@ -2,11 +2,22 @@ package Java;
 
 public class Oops {
     public static void main(String args[]){
-
+        Student s1 = new Student();
+        Student s2 = new Student(95);
+        Student s3 = new Student(22010041036L);
     }
 }
 class Student{
     int mark;
-    int roll_no;
-    
+    long roll_no;
+    Student(){
+        System.out.println("This is default constructor");
+    }
+    Student(int mark){
+        this.mark = mark;
+    }
+    Student(long roll_no){
+        this.roll_no = roll_no;
+    }
+
 }
