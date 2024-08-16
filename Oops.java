b636905782cc106fdfeb31b5b4a4a801a84f8dd5
Keshav-gola <keshavgola285@gmail.com>
2024-08-16 22:50:16 +0530

    Using Concept of Copy constructor

--- Oops.java
+++ Oops.java
@@ -3,8 +3,11 @@ package Java;
 public class Oops {
     public static void main(String args[]){
         Student s1 = new Student();
+        s1.mark = 95;
+        s1.roll_no = 22010041046L;
         Student s2 = new Student(95);
         Student s3 = new Student(22010041036L);
+        Student copy = new Student(s1);
     }
 }
 class Student{
@@ -18,6 +21,10 @@ class Student{
     }
     Student(long roll_no){
         this.roll_no = roll_no;
+    }
+    Student (Student s1){
+        this.mark = s1.mark;
+        this.roll_no = s1.roll_no;
     }
 
 }
