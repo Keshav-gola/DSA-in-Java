5cd2c15ae3691788275b04839e614248f7b35fb3
Keshav-gola <keshavgola285@gmail.com>
2024-08-17 23:37:09 +0530

    Adding new function - addFrist in LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,7 @@
 package Java;
 
 public class LinkedList {
-    class Node{
+    public static class Node{
         int data;
         Node next;
 
@@ -10,7 +10,24 @@ public class LinkedList {
             this.next = null;
         }
     }
+    public static Node head;
+    public static Node tail;
+    
+    public void addFirst(int data){
+        //step 1 = create new Node
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        //step 2 - newNode next = head
+        newNode.next = head;
+        //step3 - head = newNode
+        head = newNode;
+    }
     public static void main(String args[]){
-
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
     }
 }
