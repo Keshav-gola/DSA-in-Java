8623517737b9aaab620711ac9e149abb1a29866e
Keshav-gola <keshavgola285@gmail.com>
2024-08-18 06:01:58 +0530

    Adding new function - RemoveFirst-> node

--- LinkedList.java
+++ LinkedList.java
@@ -12,10 +12,12 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
     
     public void addFirst(int data){
         //step 1 = create new Node
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -25,8 +27,25 @@ public class LinkedList {
         //step3 - head = newNode
         head = newNode;
     }
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LinkedList is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
     public void addLast(int data){
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -41,13 +60,18 @@ public class LinkedList {
         }
         Node temp = head;
         while(temp != null){
-            System.out.println(temp.data + " ");
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
         System.out.println();
     }
     public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
 
@@ -62,10 +86,12 @@ public class LinkedList {
     }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
         ll.addFirst(2);
+        ll.addFirst(1);
         ll.addLast(3);  
         ll.addLast(4); 
+        ll.print();
+        ll.removeFirst();
         ll.print();
         
     }
