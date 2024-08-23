197d5598f68cd5dd0aee616a67656124adfd547f
Keshav-gola <keshavgola285@gmail.com>
2024-08-23 22:41:44 +0530

    Creating new File for Advance LinkedList - FloyidCycle

---
+++ AdvLinklist.java
@@ -0,0 +1,40 @@
+package Java;
+
+public class AdvLinklist {
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static Node size;
+
+    public static boolean floyidCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+       // This link-list is just created for use of floyid Cycle
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        System.out.println(floyidCycle());
+    }
+}
