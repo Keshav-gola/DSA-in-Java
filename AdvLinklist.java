002e578529d4e3ea85c43eac3373a9c34fb6a1e0
Keshav-gola <keshavgola285@gmail.com>
2024-08-24 06:14:20 +0530

    Adding new function - Remove Cycle

--- AdvLinklist.java
+++ AdvLinklist.java
@@ -28,13 +28,40 @@ public class AdvLinklist {
         }
         return false;
     }
+    public static void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return;
+        }
+        slow = head;
+        Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        prev.next = null;
+    }
 
     public static void main(String args[]){
        // This link-list is just created for use of floyid Cycle
         head = new Node(1);
-        head.next = new Node(2);
+        Node temp = new Node(2);
+        head.next = temp;
         head.next.next = new Node(3);
-        head.next.next.next = head;
+        head.next.next.next = temp;
+        System.out.println(floyidCycle());
+        removeCycle();
         System.out.println(floyidCycle());
     }
 }
