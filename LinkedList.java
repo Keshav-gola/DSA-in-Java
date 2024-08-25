5b37902b89b382ae04fa14459b1b84a15e330e2d
Keshav-gola <keshavgola285@gmail.com>
2024-08-25 21:36:09 +0530

    Adding new function - MergeSort

--- LinkedList.java
+++ LinkedList.java
@@ -202,29 +202,96 @@ public class LinkedList {
             curr = next;
         }
         head = prev;
-    }  
+    } 
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //mid node
+    }
+
+    private Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }
+            else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+    public Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        //find mid
+        Node mid = getMid(head);
+        //left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        //merge
+        return merge(newLeft, newRight);
+    } 
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
+        // ll.addFirst(1);
+        // ll.addFirst(1);
+        // ll.addLast(2);  
+        // ll.addLast(2); 
+        // ll.addLast(1);
+        // ll.addLast(1);
+        // ll.print();
+        // ll.removeFirst();
+        // ll.print();
+        // ll.removeLast();
+        // ll.print();
+        // int idx = ll.search(3);
+        // System.out.println(idx);
+        // int recIdx = ll.recSerch(3);
+        // System.out.println(recIdx);
+        // ll.reverse();
+        // ll.print();
+        // ll.deletNthfromEnd(2);
+        // ll.print();
+        // System.out.println(ll.checkPalindrome());
+
+        // LinkedList ll2 = new LinkedList();
         ll.addFirst(1);
-        ll.addFirst(1);
-        ll.addLast(2);  
-        ll.addLast(2); 
-        ll.addLast(1);
-        ll.addLast(1);
+        ll.addFirst(2);    
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
         ll.print();
-        ll.removeFirst();
-        ll.print();
-        ll.removeLast();
-        ll.print();
-        int idx = ll.search(3);
-        System.out.println(idx);
-        int recIdx = ll.recSerch(3);
-        System.out.println(recIdx);
-        ll.reverse();
-        ll.print();
-        ll.deletNthfromEnd(2);
-        ll.print();
-        System.out.println(ll.checkPalindrome());
+        ll.head = ll.mergeSort(ll.head);
+        ll.print(); 
+
         
     }
 }
