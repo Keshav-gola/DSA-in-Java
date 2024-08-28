ec948d779452acc64bca82145c968c7237f846be
Keshav-gola <keshavgola285@gmail.com>
2024-08-28 23:06:16 +0530

    Adding new folder for Stack-> new file for Stack implt Arrlist

---
+++ Stack/StackArrlist.java
@@ -0,0 +1,44 @@
+package Java.Stack;
+import java.util.ArrayList;
+public class StackArrlist {
+    static class Stack{
+        static ArrayList <Integer> list = new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()- 1);
+        }
+    }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
