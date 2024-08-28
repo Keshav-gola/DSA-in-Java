1cf126ad2c19675c9595dfe7668014f330112936
Keshav-gola <keshavgola285@gmail.com>
2024-08-28 23:40:50 +0530

    Adding new file-> Stack implt Collection

---
+++ Stack/StackCollection.java
@@ -0,0 +1,17 @@
+package Java.Stack;
+import java.util.Stack;
+public class StackCollection {
+    
+    public static void main(String args[]){
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
