930bbd7b9f87c200544e57f35ad41c01d6dabce3
Keshav-gola <keshavgola285@gmail.com>
2024-08-29 22:34:31 +0530

    Adding new function-> ReverseStack

--- Stack/StackCollection.java
+++ Stack/StackCollection.java
@@ -10,6 +10,33 @@ public class StackCollection {
         pushAtBottom(s, data);
         s.push(top);
     }
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
+    }
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+    public static void printStack(Stack <Integer> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
     public static void main(String args[]){
         Stack <Integer> s = new Stack<>();
         s.push(1);
@@ -21,6 +48,10 @@ public class StackCollection {
             System.out.println(s.peek());
             s.pop();
         }
-
+        String str = "Keshav";
+        String reuslt = reverseString(str);
+        System.out.println(reuslt);
+        reverseStack(s);
+        printStack(s);
     }
 }
