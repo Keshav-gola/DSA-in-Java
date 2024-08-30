d7b0fb2c739be1b3a2b00879d9220e2c631ef9c0
Keshav-gola <keshavgola285@gmail.com>
2024-08-30 22:46:03 +0530

    Solving new Question -> Stock_Span

--- Stack/StackCollection.java
+++ Stack/StackCollection.java
@@ -37,6 +37,26 @@ public class StackCollection {
             System.out.println(s.pop());
         }
     }
+    public static void stockSpan(int stocks[], int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for(int i = 1; i<stocks.length; i++){
+            int currPrice = stocks[i];
+            while(!s.isEmpty() && currPrice > stocks[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i] = i -prevHigh;
+            }
+            s.push(i); 
+        }
+    }
     public static void main(String args[]){
         Stack <Integer> s = new Stack<>();
         s.push(1);
@@ -53,5 +73,11 @@ public class StackCollection {
         System.out.println(reuslt);
         reverseStack(s);
         printStack(s);
+        int stock[] = {100,80,60,70,60,85,100};
+        int span[] = new int[stock.length];
+        stockSpan(stock, span);
+        for(int i =0; i<span.length; i++){
+            System.out.print(span[i]+ " ");
+        }
     }
 }
