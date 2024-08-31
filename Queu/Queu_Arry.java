4ad678cf5b774cbac2a8624d56c714a218dfe5a9
Keshav-gola <keshavgola285@gmail.com>
2024-08-31 23:28:50 +0530

    Creating Queue in main fun() using Queue Class obj

--- Queu/Queu_Arry.java
+++ Queu/Queu_Arry.java
@@ -50,7 +50,15 @@ public class Queu_Arry {
         }
     }
     public static void main(String args[]){
-         
+         Queue q = new Queue(5);
+         q.add(1);
+         q.add(2);
+         q.add(3);
+
+         while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+         }
     }
     
 }
