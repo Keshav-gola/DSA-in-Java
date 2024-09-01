5e6d454f0f9ec671d4ee39f97a3cdb802525b651
Keshav-gola <keshavgola285@gmail.com>
2024-09-01 23:15:10 +0530

    Solving new Question-> Area of Histogram

--- Stack/StackCollection.java
+++ Stack/StackCollection.java
@@ -128,6 +128,51 @@ public class StackCollection {
         }
         return false;
     }
+    public static void maxArea(int arr[]){
+        int maxArea = 0;
+        int nsr[] = new int [arr.length];
+        int nsl[] = new int [arr.length];
+        //Next Smaller Right
+        Stack <Integer> s = new Stack<>();
+
+        for(int i = arr.length-1; i>= 0; i--){
+            while (!s.isEmpty() && arr[s.peek()]  >= arr[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i] = arr.length;
+            }
+            else{
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+        //Next Smaller left
+        s = new Stack<>();
+
+        for(int i = 0; i< arr.length; i++){
+            while (!s.isEmpty() && arr[s.peek()]  >= arr[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i] = -1;
+            }
+            else{
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //Current Area : width = j-i-1 = nsr[i] - nsl[i] -1
+        for(int i = 0; i<arr.length; i++){
+            int heigth = arr[i];
+            int width = nsr[i] - nsl[i] -1;
+            int currArea = heigth * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+        System.out.println("max area in histogram = " + maxArea);
+
+    }
 
     public static void main(String args[]){
         Stack <Integer> s = new Stack<>();
@@ -174,5 +219,9 @@ public class StackCollection {
 
         System.out.println(isDuplicate(tr));
         System.out.println(isDuplicate(fl));
+
+        int histogram[] = {2,1,5,6,2,3};
+        maxArea(histogram);
+        
     }
 }
