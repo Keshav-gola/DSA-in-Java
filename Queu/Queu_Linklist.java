04131641362cdc3156605c95c883b7fdec694dfe
Keshav-gola <keshavgola285@gmail.com>
2024-09-05 23:15:29 +0530

    Adding add & remove fun() for linklist queu

--- Queu/Queu_Linklist.java
+++ Queu/Queu_Linklist.java
@@ -1,8 +1,53 @@
 package Java.Queu;
 
 public class Queu_Linklist {
-    static class Queue{
+    static class Node{
+        int data;
+        Node next;
         
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Queue{
+        static Node head = null;
+        static Node tail = null;
+
+
+        public static boolean isEmpty(){
+            return head == null & tail == null;
+        }
+
+        //add
+        public static void add(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty queue");
+                return -1;
+            }
+
+            int front = head.data;
+            if(tail == head){
+                tail = head = null;
+            }
+            else{
+                head = head.next;
+            }
+            return front;
+        }
+        
+
     }
     
 }
