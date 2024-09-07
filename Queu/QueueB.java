910c48a8d619a05173e45af9636bf842ce5214ed
Keshav-gola <keshavgola285@gmail.com>
2024-09-07 23:24:01 +0530

    Adding new class for sloving new Qus using stack with collection

---
+++ Queu/QueueB.java
@@ -0,0 +1,51 @@
+package Java.Queu;
+
+import java.util.*;
+
+public class QueueB {
+    static class Stack{
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty(){
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public static void push(int data){
+            if(!q1.isEmpty()){
+                q1.add(data);
+            }
+            else{
+                q2.add(data);
+            }
+        }
+
+        public static int pop() {
+            if(isEmpty()){
+                System.out.println("Empty stack");
+                return -1;
+            }
+            int top = -1;
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                    top = q1.remove();
+                    if(q1.isEmpty()){
+                        break;
+                    }
+                    q2.add(top);
+                }
+            }
+            else{
+                while(!q2.isEmpty()){
+                    top = q2.remove();
+                    if(q2.isEmpty()){
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+        
+    }
+}
