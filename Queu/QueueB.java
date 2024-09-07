bff0718bc63b219c2305743f721a5ef455dfcbcb
Keshav-gola <keshavgola285@gmail.com>
2024-09-07 23:30:51 +0530

    Implementing Queue using Stack

--- Queu/QueueB.java
+++ Queu/QueueB.java
@@ -69,4 +69,15 @@ public class QueueB {
             return top;
         }
     }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }
