eb12f62ee819c855151cf97283868487003f5d53
Keshav-gola <keshavgola285@gmail.com>
2024-09-09 23:10:49 +0530

    Solving a Question to printNonRepating elements in String

--- Queu/Question.java
+++ Queu/Question.java
@@ -1,5 +1,29 @@
 package Java.Queu;
 import java.util.*;
 public class Question {
-    
+    public static void printNonRepating(String str){
+        int freq [] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[q.peek()-'a'] > 1){
+                q.remove();
+            }
+
+            if(q.isEmpty()) {
+                System.out.print(-1+" ");
+            }else{
+                System.out.print(q.peek()+" ");
+            }
+        }
+        System.out.println();
+    } 
+    public static void main(String args[]){
+        String str = "aabccxb";
+        printNonRepating(str);
+    }
 }
