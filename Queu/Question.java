7d7c0f5a6857fca8874a0a164cbdfcd16be77f72
Keshav-gola <keshavgola285@gmail.com>
2024-09-12 23:54:43 +0530

    Solving new question to reverse a Queue

--- Queu/Question.java
+++ Queu/Question.java
@@ -1,6 +1,16 @@
 package Java.Queu;
 import java.util.*;
 public class Question {
+    public static void reverse(Queue<Integer> q){
+        Stack <Integer> s = new Stack<>();
+
+        while(!q.isEmpty()){
+            s.push(q.remove());
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        } 
+    }
     public static void printNonRepating(String str){
         int freq [] = new int[26];
         Queue<Character> q = new LinkedList<>();
@@ -25,5 +35,19 @@ public class Question {
     public static void main(String args[]){
         String str = "aabccxb";
         printNonRepating(str);
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        reverse(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove()+ " ");
+        }
+        System.out.println();
+
     }
 }
